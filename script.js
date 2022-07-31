39671848bc2f36070cdba4889df6b949df43a4fc
Zachary Landry <landryzachary4@gmail.com>
2022-07-31 13:49:29 -0400

    updates made to all pages

--- script.js
+++ script.js
@@ -3,47 +3,58 @@ const deck = ["A Clubs", "2 Clubs", "3 Clubs", "4 Clubs", "5 Clubs", "6 Clubs", 
     "A Spades", "2 Spades", "3 Spades", "4 Spades", "5 Spades", "6 Spades", "7 Spades", "8 Spades", "9 Spades", "10 Spades", "J Spades", "Q Spades", "K Spades",
     "A Diamonds", "2 Diamonds", "3 Diamonds", "4 Diamonds", "5 Diamonds", "6 Diamonds", "7 Diamonds", "8 Diamonds", "9 Diamonds", "10 Diamonds", "J Diamonds", "Q Diamonds", "K Diamonds"];
 
-const userCard = deck[Math.floor(Math.random() * deck.length)];
-const userCard2 = deck[Math.floor(Math.random() * deck.length)];
-const cpuCard = deck[Math.floor(Math.random() * deck.length)];
-const cpuCard2 = deck[Math.floor(Math.random() * deck.length)];
+const user1 = [];
+const user2 = [];
+// user1.forEach(getSum);
+// const element = document.getElementById("sum");
+// document.getElementById("user1").innerHTML = sum;
+
+
+const user1Card = deck[Math.floor(Math.random() * deck.length)];
+const user1Card2 = deck[Math.floor(Math.random() * deck.length)];
+const user2Card = deck[Math.floor(Math.random() * deck.length)];
+const user2Card2 = deck[Math.floor(Math.random() * deck.length)];
 const newCard = deck[Math.floor(Math.random() * deck.length)];
 
 
-// function user(){
+// function user1(){
 //     return hand = [userCard, userCard2]
 // }
 
-// function cpu(){
+// function user2(){
 //     return hand = [userCard, userCard2]
 // }
-// user();
-// cpu();
+// user1();
+// user2();
 // console.log(hand)
 
-function userHand() {
-  return user = [userCard, userCard2];
+function user1Hand() {
+  return user1 = [user1Card, user1Card2];
 }
+user1.push(user1Hand);
+console.log(user1);
 
-function cpuHand() {
-    return cpu = [cpuCard, cpuCard2];
+function user2Hand() {
+    return user2 = [user2Card, user2Card2];
 }
-userHand();
-cpuHand();
-console.log(user);
-console.log(cpu);
-
-
+user2.push(user2Hand)
 
 function startGame(){
-
+    user1Hand();
+    user2Hand();
+    //add user getSum
 }
     
 function hitMe(){
     return card = [newCard]
+    //need to push to either user
 }
-hitME();
+
 
 function stay(){
+    //need stop function that is adding cards
+}
 
+function getSum(){
+    sum += values;
 }
