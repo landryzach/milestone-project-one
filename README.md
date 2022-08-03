ca6dfc688a9228b37d36fa86291b4db9f93be794
Zachary Landry <landryzachary4@gmail.com>
2022-08-02 21:24:49 -0400

    updates to full game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # milestone-project-one
 I chose blackjack, becuase it seems like a simple game to build. This is my first solo project from scratch and we have a short period of time to create the project, so a simple game seemed logical. Also, blackjack is the only casino game I've actually played and gambled on, so there is the smallest bit of person history there as well.
+
--- script.js
+++ script.js
@@ -3,46 +3,102 @@ const deck = ["A Clubs", "2 Clubs", "3 Clubs", "4 Clubs", "5 Clubs", "6 Clubs", 
     "A Spades", "2 Spades", "3 Spades", "4 Spades", "5 Spades", "6 Spades", "7 Spades", "8 Spades", "9 Spades", "10 Spades", "J Spades", "Q Spades", "K Spades",
     "A Diamonds", "2 Diamonds", "3 Diamonds", "4 Diamonds", "5 Diamonds", "6 Diamonds", "7 Diamonds", "8 Diamonds", "9 Diamonds", "10 Diamonds", "J Diamonds", "Q Diamonds", "K Diamonds"];
 
-const user1 = [];
-const user2 = [];
-// user1.forEach(getSum);
-// const element = document.getElementById("sum");
-// document.getElementById("user1").innerHTML = sum;
+let User1 = [];
+let User2 = [];
+let user1Score = 0;
+let user2Score = 0;
+let hit = true;
 
 
-const user1Card = deck[Math.floor(Math.random() * deck.length)];
-const user1Card2 = deck[Math.floor(Math.random() * deck.length)];
-const user2Card = deck[Math.floor(Math.random() * deck.length)];
-const user2Card2 = deck[Math.floor(Math.random() * deck.length)];
-const newCard = deck[Math.floor(Math.random() * deck.length)];
-
-function user1Hand() {
-  return user1 = [user1Card, user1Card2];
-}
-user1.push(user1Hand);
-console.log(user1);
-
-function user2Hand() {
-    return user2 = [user2Card, user2Card2];
-}
-user2.push(user2Hand)
+function shuffle() {
+    for(let i=deck.length - 1; i > 0; i--){
+        let d = Math.floor(Math.random() * i)
+        let x = deck[i];
+        deck[i] = deck[d];
+        deck[d] = x;
+    }
+} //got framing of this code from https://www.programiz.com/javascript/examples/shuffle-card
+//shuffle is complete and is working
 
 function startGame(){
-    user1Hand();
-    user2Hand();
-    getSum();
-}
+    for(let i=0; i<2; i++){
+        let user1 = deck.pop();
+        User1.push(user1);
+        user1Score += getScore(user1);
+        console.log(user1);
+        console.log(user1Score);
+    } for(let i=0; i<2; i++){
+        let user2 = deck.pop();
+        User2.push(user2);
+        user2Score += getScore(user2);
+        console.log(user2);
+        console.log(user2Score);
+    }
     
-function hitMe(){
-    return card = [newCard]
-    //need to push to either user
+    document.getElementById("hitMe1").addEventListener("click", hitMe1);
+    document.getElementById("hitMe2").addEventListener("click", hitMe2);
+    document.getElementById("stay1").addEventListener("click", stay);
+    document.getElementById("stay2").addEventListener("click", stay);
+
+    document.getElementById("user1").innerText = User1;
+    document.getElementById("user2").innerText = User2;
+    document.getElementById("u1score").innerText = "User1:" + " " + user1Score;
+    document.getElementById("u2score").innerText = "User2:" + " " + user2Score;
 }
+
+function hitMe1(){
+    
+    let user1 = deck.pop();
+    user1Score += getScore(user1);
+    console.log(user1, "hit")
+}
+
+function hitMe2(){
+    let user2 = deck.pop();
+    user2Score += getScore(user2);
+    console.log(user2, "hit");
+}
+
 
 
 function stay(){
-    //need stop function that is adding cards
+    hit = false;
+
+    let result = "";
+    console.log(result, "result");
+    if (user1Score === user2Score){
+        result = "It's a tie";
+    } else if (user1Score > user2Score){
+        result = "User1 Wins!";
+    } else if (user1Score < user2Score){
+        result = "User2 Wins!";
+    }
+
+    
 }
 
-function getSum(){
-    sum += values;
+function getScore(hand){
+    let cards = hand.split(" ").slice(0,2);
+    let score = hand[0];
+    
+        if(score === "A"){
+        return 1;
+        } if(score === "J"){
+        return 10;
+        } if(score === "Q"){
+        return 10;
+        } if(score === "K"){
+        return 10;
+        } if(score === "1"){ //added lines 80 and 81 for "10" cards because hand[0] only grabs character in the element.
+        return 10;
+        } else {
+        return parseInt(score);
+    }
+} //thanks for the help on this one!
+//getScore is complete and is working
+
+window.onload = () => {
+    shuffle();
+    startGame();
 }
+
